4cfd719b8404c92d7a87d4a82b268eeafd675d33
admin <chris@postfmly.com>
2026-04-15 04:07:49 -0500

    chore: fix image name build: use env

---
+++ docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S bash -e
+
+_yellow="\e[4;93m"
+_nc="\e[0m"
+_build=📦
+_start="▶️ "
+
+echo -e "${_build} ${_yellow}Building${_nc}:\n"
+./Dockerfile
+
+echo -e "\n${_start} ${_yellow}Starting${_nc}:\n"
+docker container rm --force dropzonebot > /dev/null 2>&1
+docker container run --rm --name dropzonebot --publish 8001:8001 --env TZ=America/Chicago --detach dropzonebot
+
+unset _yellow
+unset _nc
+unset _build
+unset _start
