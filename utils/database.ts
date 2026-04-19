d2d01eb1bb1b8237049126f396fba26a67b64750
admin <chris@postfmly.com>
2026-04-19 15:39:45 -0500

    fix: remove id from target

--- utils/database.ts
+++ utils/database.ts
@@ -176,13 +176,10 @@ const updatePoints = async (name: string, points: number): Promise<void> => {
         points: points
       })
       .onConflictDoUpdate({
+        target: users.name,
         set: {
           points: points
-        },
-        target: [
-          users.id,
-          users.name
-        ]
+        }
       })
     // biome-ignore lint/suspicious/noExplicitAny: catch all errors
   } catch (e: any) {
